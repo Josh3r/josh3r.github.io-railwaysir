bac61b9678cc89ca5f206988e0927a871e5100cc
Test <teo.josh@yahoo.com.sg>
2019-11-13 14:49:40 +0800

    changed some package.json stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { render } from "react-dom";
+
+import Tabs from './Components/Tabs';
+require('./styles.css');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div style={{background: 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)'}}>
+      <h1 style={{color:'black',fontSize:'40px',fontWeight:'bold' ,display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
+        Railways in the Industrail Revolution
+        <img src='https://railwaywondersoftheworld.com/wpimages/wp45737414_05_06.jpg'></img>
+      </h1>
+     <Tabs style={{display:'flex',justifyContent:'center'}}>
+      <div label="Background">
+        See ya later, <em>Alligator</em>!
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, debitis laborum. Eius enim tenetur inventore ad, laboriosam soluta. Vero architecto similique voluptas natus non dolor, itaque odit aliquam excepturi laudantium.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div label="Britain">
+        After 'while, <em>Crocodile</em>!
+      </div>
+      <div label="Netherlands">
+        Nothing to see here, this tab is <em>extinct</em>!
+      </div>
+    </Tabs>
     </div>
   );
 }
+
+
 
 export default App;
